f1a4b7e13edf57d54879658ce122a32f46766a08
mjkchoi1 <mjc@njit.edu>
2024-10-31 08:11:47 -0400

    with gRPC

--- core-management/src/archiveFunctionVersion.py
+++ core-management/src/archiveFunctionVersion.py
@@ -1,21 +1,12 @@
-# archiveFunctionVersion.py
-
 from restoreArchivedVersion import functions
 
 def archiveFunctionVersion(function_name: str, version: str) -> bool:
-
-
-    
-
     if function_name not in functions:
-        return False
-
+        functions[function_name] = {}
     if version not in functions[function_name]:
         return False
-
     if functions[function_name][version] == 'archived':
         return False
-
     functions[function_name][version] = 'archived'
     return True
 
